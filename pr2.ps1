6f47528729773f5d921a2142ce2fd5555024e998
Anna Karutina <anna.karutina@khk.ee>
2024-11-11 15:28:13 +0200

    Practice 2 - arrays and hashtables

---
+++ pr2.ps1
@@ -0,0 +1,13 @@
+﻿$ht1 = @{"Name"="John","Joe", "Mary"; "DayWorked"=12, 20, 18}
+$ht2 = @{"Name"="John","Joe", "Mary"; "SalaryPerDay"=100, 120, 150}
+
+$salary = @()
+
+$salary += $ht1.DayWorked[0] * $ht2.SalaryPerDay[0]
+$salary += $ht1.DayWorked[1] * $ht2.SalaryPerDay[1]
+$salary += $ht1.DayWorked[2] * $ht2.SalaryPerDay[2]
+
+
+$ht3 = @{"Name"=$ht1.Name; "Salary"=$salary}
+
+$ht3
