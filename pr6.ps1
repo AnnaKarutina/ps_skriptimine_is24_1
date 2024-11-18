ce7c6eb51315d9412210ce0f4eba3d9ec97803a2
Anna Karutina <anna.karutina@khk.ee>
2024-11-18 12:04:15 +0200

    Practice 6 - write created CSV data to file

---
+++ pr6.ps1
@@ -0,0 +1,19 @@
+﻿$students = Import-Csv C:\Users\Anna\ps_skriptimine\ages.csv
+
+$schools = @()
+$names = @()
+$schoolNames = @()
+
+
+foreach($student in $students){
+    $school = ""
+    if([int32]$student.Age -ge 4 -and [int32]$student.Age -le 10){
+        $school = "Junior"   
+    } elseif ([int32]$student.Age -ge 11 -and [int32]$student.Age -le 17) {
+        $school = "Senior"
+    } 
+    $schools += [PSCustomObject]@{"Name"=$student.Name; "School"=$school}
+    
+}
+
+$schools | Select-Object -Property Name, School | Export-Csv -NoTypeInformation C:\Users\Anna\ps_skriptimine\schools.csv 
