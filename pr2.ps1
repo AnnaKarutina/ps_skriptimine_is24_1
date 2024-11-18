60383d95d56baf5ec1279120d380372b3b50ee0b
Anna Karutina <anna.karutina@khk.ee>
2024-11-18 09:08:23 +0200

    Practice 2 - separately data arrays and hashtables

--- pr2.ps1
+++ pr2.ps1
@@ -1,11 +1,15 @@
-﻿$ht1 = @{"Name"="John","Joe", "Mary"; "DayWorked"=12, 20, 18}
-$ht2 = @{"Name"="John","Joe", "Mary"; "SalaryPerDay"=100, 120, 150}
+﻿$names = @("John","Joe", "Mary")
+$dayWorked = @(12, 20, 18)
+$salaryPerDay = @(100, 120, 150)
+
+$ht1 = @{"Name"=$names; "DayWorked"=$dayWorked}
+$ht2 = @{"Name"=$names; "SalaryPerDay"=$salaryPerDay}
 
 $salary = @()
 
-$salary += $ht1.DayWorked[0] * $ht2.SalaryPerDay[0]
-$salary += $ht1.DayWorked[1] * $ht2.SalaryPerDay[1]
-$salary += $ht1.DayWorked[2] * $ht2.SalaryPerDay[2]
+for($i = 0; $i -lt $names.Count; $i++){
+    $salary += $ht1.DayWorked[$i] * $ht2.SalaryPerDay[$i]
+}
 
 
 $ht3 = @{"Name"=$ht1.Name; "Salary"=$salary}
